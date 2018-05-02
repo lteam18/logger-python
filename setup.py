ec85e8a008acfe31c336ee452840baa6e850a93e
Edwin.JH.Lee <edwin.jh.lee@gmail.com>
2018-05-03 01:11:20 +0800

    ready

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+      name="logger",
+      version="0.1",
+      description="logger",
+      author="Edwin.JH.Lee",
+      url="http://github.com/lteam/logger-python",
+      license="Apache Lisence 2",
+      packages= find_packages(),
+      scripts=["src/logger.py"],
+      )
---
+++ src/logger.py
@@ -0,0 +1,4 @@
+class Logger(object):
+    def __init__(self):
+        self.info = lambda msg: print(msg)
+    
