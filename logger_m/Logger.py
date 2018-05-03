aebfe296362c3b065b2de5b9441d1b902305059b
Edwin.JH.Lee <edwin.jh.lee@gmail.com>
2018-05-03 13:06:07 +0800

    Using function to replace LevelLogger. About #3

--- logger_m/Logger.py
+++ logger_m/Logger.py
@@ -1,10 +1,23 @@
-import LevelLogger
 
 class Logger(object):
     def __init__(self):
-        self.debug = LevelLogger()
-        self.info = LevelLogger()
-        self.warn = LevelLogger()
-        self.error = LevelLogger()
-        self.fatal = LevelLogger()
+        pass
+
+    def _writeLog(self, level, msg = None, data = None, trace = None):
+        pass
+
+    def debug(self, msg = None, data = None, trace = None):
+        self._writeLog(0, msg, data, trace)
+
+    def info(self, msg = None, data = None, trace = None):
+        self._writeLog(1, msg, data, trace)
+
+    def warn(self, msg = None, data = None, trace = None):
+        self._writeLog(2, msg, data, trace)
+
+    def error(self, msg = None, data = None, trace = None):
+        self._writeLog(3, msg, data, trace)
+
+    def fatal(self, msg = None, data = None, trace = None):
+        self._writeLog(4, msg, data, trace)
 
