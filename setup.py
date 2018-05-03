83a59f896111bc2e7a6157807c1090af75cc622f
Edwin.JH.Lee <edwin.jh.lee@gmail.com>
2018-05-03 12:08:24 +0800

    w

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
       name="loggerxx",
-      version="0.11",
+      version="0.12",
       description="logger",
       author="Edwin.JH.Lee",
       url="http://github.com/lteam/logger-python",
