7129a5d209ff8d592451fa9a79cf1e0c2230ad9a
Edwin.JH.Lee <edwin.jh.lee@gmail.com>
2018-05-03 11:47:36 +0800

    w

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 setup(
       name="logger",
-      version="0.8",
+      version="0.9",
       description="logger",
       author="Edwin.JH.Lee",
       url="http://github.com/lteam/logger-python",
       license="Apache Lisence 2",
-      packages= find_packages()
+      packages= find_packages(),
+      scripts=["loggerx.py"],
       )
