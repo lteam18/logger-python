83df3caa12db3c825b3b934b110dff523f146eff
amslime <756433605@qq.com>
2018-05-12 07:32:50 +0800

    Create loggerTest.py
    Add simple test

---
+++ wl/src/loggerTest.py
@@ -0,0 +1,26 @@
+from logger import Logger
+import serialize.output as Output
+import serialize.Serializer as Serializer
+
+if __name__ == '__main__':
+    root_logger = Logger.createRoot('MainLogger-123', \
+                                    serializer = Serializer.combine([ \
+                                        Serializer.Major(output = Output.combine( \
+                                            [Output.CONSOLE, Output.logFile('a.log')] \
+                                            )), \
+                                        Serializer.Major(output = Output.logFile('b.log'))] \
+                                            ) \
+                                    )
+    logger = root_logger
+
+    logger.debug(msg = "Program ready")
+
+    logger.info(msg = "123", data = "a.1, b.2")
+    
+    sub_logger = root_logger.createSub('sublogger')
+
+
+    
+
+    
+
